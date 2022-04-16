810703e9d249cb143529f41b614da9c5aa42b357
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-04-16 09:54:47 -0600

    Ecomm App cart functionality

---
+++ 15_ecomm/routes/carts.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cartsRepo = require('../repositories/Carts');
+
+const router = express.Router();
+
+// Receive a post request to add an item to a cart
+router.post('/cart/products', async (req, res) => {
+	// Figure out the cart
+	let cart;
+	if (!req.session.cartId) {
+		// We dont have a cart, we need to create one,
+		// and store the cart id on the req.session.cartId property
+		cart = await cartsRepo.create({ items: [] });
+		req.session.cartId = cart.id;
+	} else {
+		// We have a cart! Lets get it from the repository
+		cart = await cartsRepo.getOne(req.session.cartId);
+	}
+
+	// Either increment quantity for existing product
+	// OR add new product to items array
+	const existingItem = cart.items.find(
+		(item) => item.id === req.body.productId
+	);
+	if (existingItem) {
+		// increment quantity and save cart
+		existingItem.quantity++;
+	} else {
+		// add new product if to items array
+		cart.items.push({ id: req.body.productId, quantity: 1 });
+	}
+	await cartsRepo.update(cart.id, {
+		items: cart.items,
+	});
+
+	res.send('Product added to cart');
+});
+
+// Receive a get request to show all items in the cart
+// Receive a post request to delete an item from the cart
+
+module.exports = router;
